0581b5bf8112e53e65fac6eed6898a98d9ed0d80
DXSKULL <dxskull9@gmail.com>
2024-07-29 13:03:48 +0500

    step37

---
+++ javascript/step37.js
@@ -0,0 +1,6 @@
+// Step 37
+// Your loop should now run eight times. Inside the body of the loop, print the value of the i iterator and see what happens.
+const count = 8;
+for (let i = 0; i < count; i = i + 1) {
+    console.log(i)
+  }
