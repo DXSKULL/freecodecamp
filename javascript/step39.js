48b11191075a65794f7f3e6c1db5c53851b79884
DXSKULL <dxskull9@gmail.com>
2024-07-29 13:06:16 +0500

    step39

---
+++ javascript/step39.js
@@ -0,0 +1,6 @@
+// Step 39
+// Unfortunately, now you cannot see what your loop is doing.
+
+// Use let to declare a result variable, and assign it an empty string. An empty string is represented by quotation marks with nothing between them, such as "".
+
+let result = "";
